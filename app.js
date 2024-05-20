041d24e09fc470a54a1c49c8dbcf3af6a89727e6
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-05-20 11:35:22 +0530

    updated

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const maxPaletteBoxes = 32;
 
 const generatePalette = ()=>{
 
+    container.innerHTML = "";
+
     for (let i = 0;i < maxPaletteBoxes; i++) {
         
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
@@ -14,12 +16,31 @@ const generatePalette = ()=>{
         color.classList.add("color");
         color.innerHTML = `<div class="rect-box" style = "background : ${randomHex}"></div>
                             <span class="hex-value">${randomHex}</span>`;
+
+
+        color.addEventListener("click", () => copyColor(color, randomHex));
+
+        container.appendChild(color);
         
     }
 
 
 
 
+}
+
+generatePalette();
+
+const copyColor =  (elem , hexValue ) =>{
+
+    const colorElement = elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexValue).then(() => {
+        colorElement.innerText = "Copied";
+
+
+        setTimeout(() => colorElement.innerText = hexValue, 500);
+
+    }).catch(() => alert("Falied to copy the color code!"));
 }
 
 refreshButton.addEventListener("click", generatePalette)
